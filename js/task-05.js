e2828506ed4fa130c78235cc201b1b78b19e47f4
Olena <petrova1512@gmail.com>
2023-06-21 20:56:25 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,11 @@ const inputRef = document.querySelector("#name-input");
 const nameOutputRef = document.querySelector("#name-output");
 
 const onTextInput = (event) => {
-  nameOutputRef.textContent = event.currentTarget.value;
+  if (event.currentTarget.value !== "") {
+    nameOutputRef.textContent = event.currentTarget.value;
+  } else {
+    nameOutputRef.textContent = "Anonymous";
+  }
 };
 
 inputRef.addEventListener("input", onTextInput);
